16c57c1d20f6dda8cf9c51a6107451c6566f353d
Brian Rossetti <brossetti1@gmail.com>
2022-03-30 14:49:59 -0600

    footer update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,15 @@
-import { Flex, FlexProps, Text } from '@chakra-ui/react';
+import { Flex, FlexProps, Link } from '@chakra-ui/react';
 import React from 'react';
 
 const Footer = (props: FlexProps) => {
 	return (
 		<Flex {...props} gap={10} justifyContent="space-around">
-			<Text>Test One</Text>
-			<Text>Test Two</Text>
+			<Link ml={10} href="#">
+				Link One
+			</Link>
+			<Link ml={10} href="#">
+				Link Two
+			</Link>
 		</Flex>
 	);
 };
