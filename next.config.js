45f56568ab745495c1d4f665051cf987cd33b2df
brianrossetti <brossetti1@gmail.com>
2022-11-21 11:05:04 -0500

    removing intl

--- next.config.js
+++ next.config.js
@@ -2,15 +2,6 @@
  * @type {import('next').NextConfig}
  **/
 
-const i18n = {
-	// These are all the locales you want to support in
-	// your application
-	locales: ['en', 'fr', 'es'],
-	// This is the default locale you want to be used when visiting
-	// a non-locale prefixed path e.g. `/hello`
-	defaultLocale: 'en',
-};
-
 const nextConfig = {
 	reactStrictMode: true,
 	webpack(config) {
@@ -24,7 +15,6 @@ const nextConfig = {
 
 		return config;
 	},
-	i18n,
 };
 
 module.exports = nextConfig;
